dbf6bbf4b77760dad34dbbc7a2734877604797c4
Christer Miroy <chrstrzm.mry@gmail.com>
2024-01-31 02:31:34 +0800

    contact page layout design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,28 @@
+import Image from 'next/image'
+import styles from './contact.module.css'
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/message.png"
+          alt="message"
+          fill
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <h1>Contact us</h1>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="tel" name="tel" placeholder="Phone Number(Optional)" />
+          <textarea name="message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default Contact
