53f0131a2a6cca188f5873f2ba0471f1c6f2e97e
Christer Miroy <chrstrzm.mry@gmail.com>
2024-02-05 16:53:43 +0800

    fetch data from a given URL

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,24 @@
 import Image from 'next/image'
 import styles from './singlePost.module.css'
+import PostUser from '@/components/postUser/postUser';
+import { Suspense } from 'react';
 
-const SinglePost = () => {
+const getData = async (slug) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${slug}`
+  );
+
+  if (!res.ok) {
+    throw new Error('Something went wrong!');
+  }
+
+  return res.json();
+};
+
+const SinglePost = async ({params}) => {
+  const {slug} = params;
+  const post = await getData(slug);
+  
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
@@ -13,7 +30,7 @@ const SinglePost = () => {
         />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             src="https://images.pexels.com/photos/1862402/pexels-photo-1862402.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
@@ -22,17 +39,15 @@ const SinglePost = () => {
             width={50}
             height={50}
           />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>John Doe</span>
-          </div>
+          <Suspense fallback={<div>Loading...</div>} />
+          <PostUser userId={post.userId} />
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
         <div className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta vero facilis facere possimus at eaque illo quod tempore consequatur minus reprehenderit ab, impedit molestiae obcaecati rerum enim! Nulla, aperiam eos.
+          {post.body}
         </div>
       </div>
     </div>
--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import styles from './postCard.module.css'
 import Link from 'next/link'
 
-const PostCard = () => {
+const PostCard = ({post}) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -17,9 +17,9 @@ const PostCard = () => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut deserunt quae earum qui, eos voluptatum voluptas architecto laboriosam, obcaecati sunt, ea quaerat doloremque non officia laudantium error at tenetur beatae?</p>
-        <Link href="/blog/post" className={styles.link}>Read More</Link>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link href={`/blog/${post.id}`} className={styles.link}>Read More</Link>
       </div>
     </div>
   );
