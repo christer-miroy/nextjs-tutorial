44b9da1bc26ebcb58c08d035a3e40ad46da83318
Christer Miroy <chrstrzm.mry@gmail.com>
2024-02-07 01:00:51 +0800

    delete blog API

--- src/app/api/blog/[slug]/route.js
+++ src/app/api/blog/[slug]/route.js
@@ -6,10 +6,22 @@ export const GET = async (request, { params }) => {
   const { slug } = params;
   try {
     connectToDB();
-    const post = await Post.findOne({slug});
+    const post = await Post.findOne({ slug });
     return NextResponse.json(post);
   } catch (error) {
     console.log(error);
     throw new Error('Failed to fetch post');
+  }
+};
+
+export const DELETE = async (request, { params }) => {
+  const { slug } = params;
+  try {
+    connectToDB();
+    await Post.deleteOne({ slug });
+    return NextResponse.json("Post deleted");
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to delete post');
   }
 };
