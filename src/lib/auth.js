2150855809727dfe16b2097f2620aa7eca21a574
Christer Miroy <chrstrzm.mry@gmail.com>
2024-02-07 15:14:39 +0800

    login using github

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { connectToDB } from './utils';
+import { User } from './models';
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,4 +14,31 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(user, account, profile);
+
+      if (account.provider === 'github') {
+        connectToDB();
+        try {
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              img: profile.avatar_url,
+            });
+
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+
+      return true;
+    },
+  },
 });
